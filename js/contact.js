eb5b8a0fcdc45544d155256add2f7d644177df7e
raheel <mraheel075@gmail.com>
2024-07-20 11:13:31 +0500

    update

--- js/contact.js
+++ js/contact.js
@@ -5,11 +5,11 @@ $(function () {
     // validator files are included in the download package
     // otherwise download from http://1000hz.github.io/bootstrap-validator
 
-    $('#contact-form').validator();
+    $('#contact-form-').validator();
 
 
     // when the form is submitted
-    $('#contact-form').on('submit', function (e) {
+    $('#contact-form-').on('submit', function (e) {
 
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
@@ -34,9 +34,9 @@ $(function () {
                     // If we have messageAlert and messageText
                     if (messageAlert && messageText) {
                         // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form-').find('.messages').html(alertBox);
                         // empty the form
-                        $('#contact-form')[0].reset();
+                        $('#contact-form-')[0].reset();
                     }
                 }
             });
